bf2ee2c57ba5505096e4397d6ed89adba94a37ad
Zeitgeisth <mrsubasht@gmail.com>
2021-07-30 15:26:41 +0545

    heroku changes

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const {Sequelize} = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL ,"articlefeed", "postgres", "root", {
+const sequelize = new Sequelize(process.env.DATABASE_URL ,"d4a2nuf0mpmnu9", "zobvhybadxlbpn", "6a09d9e9c1715caca2b7fbde59be0c8ce689451a8b5244b916b17512f432b29a", {
     host: "ec2-44-194-225-27.compute-1.amazonaws.com",
     dialect: "postgres",
     protocol: "postgres",
